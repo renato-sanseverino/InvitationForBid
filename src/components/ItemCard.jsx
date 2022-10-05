9efc8a8cd9bef4ac9bf97d963421030f42ac89a8
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-05 09:04:26 -0300

    Alteração no frontend

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { api, notification } from '../config/defaults.js'
 
 
-const changePrice = () => {
-  toast.error('Favor preencher o novo valor', notification.options);
-}
-
 export const ItemCard = ({ item }) => {
+	const [deal, setDeal] = useState(item)
+
+	const changePrice = () => {
+		toast.success('We already have the best deal R$ ' + deal.avgPrice, notification.options)
+	}
+
 	return (
 		<>
 			<Toaster/>
 
 			<div className="p-8 border-solid border-2 rounded-lg" onClick={changePrice} >
 				<h2 className="text-2xl font-bold">{item.name}</h2>
-				<p>{item.AvgPrice}</p>
+				<p>Preço Médio: R$ {item.avgPrice}</p>
 			</div>
 		</>
 	)
