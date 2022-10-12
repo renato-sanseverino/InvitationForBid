c71e628430edc46c4f71f2afdea0261b8a79b411
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-12 12:57:36 -0300

    Ajustes

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -9,9 +9,9 @@ export const ItemCard = ({ item, index, parentRef }) => {
 
 	return (
 		<>
-			<div className='inline w-72 h-52' onClick={onClick} >
-				<img className='absolute z-10 rounded-lg w-full h-full ' src={item.image} ></img>
-				<fieldset className='absolute z-20 flex flex-col justify-center' style={{'border': '0'}}>
+			<div className="inline w-72 h-52" onClick={onClick} >
+				<img className="absolute z-10 rounded-lg w-72 h-52" src={item.image} ></img>
+				<fieldset className="absolute z-20 flex flex-col justify-center" style={{'border': '0'}}>
 					<h2 className='textfield'>{item.name}</h2>
 					<p className='textfield'>Preço Médio: R$ {item.avgPrice}</p>
 					<p className='textfield'>{item.description}</p>
