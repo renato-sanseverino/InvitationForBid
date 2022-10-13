8f7942e02150550b0a6ac952b83db834c3f97029
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-13 04:40:54 -0300

    Correções no frontend

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -9,7 +9,7 @@ export const ItemCard = ({ item, index, parentRef }) => {
 	}
 
 	return (
-		<div className="w-80 h-52 rounded-lg" style={{ backgroundImage: `url(${item.image})` }} onClick={onClick} >
+		<div className="w-96 h-52 rounded-lg bg-cover bg-center" style={{ backgroundImage: `url(${item.image})` }} onClick={onClick} >
 			<fieldset className="flex flex-col justify-center">
 				<h2 className="textfield font-bold text-2xl">{item.name}</h2>
 				<p className="textfield font-bold text-xl">Preço Médio: R$ {item.avgPrice}</p>
