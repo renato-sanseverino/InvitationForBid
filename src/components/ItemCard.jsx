bbeb5c3ccb679d812e89192f9d7639f20f8faa99
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-13 03:38:42 -0300

    Correção de bugs

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -9,14 +9,12 @@ export const ItemCard = ({ item, index, parentRef }) => {
 	}
 
 	return (
-		<div style={{'var(--image-url)': item.image}}>
-			<div className='w-72 h-52 rounded-lg bg-[image:var(--image-url)]'>
-				<fieldset className="flex flex-col justify-center" onClick={onClick}>
-					<h2 className='textfield font-bold text-2xl'>{item.name}</h2>
-					<p className='textfield font-bold text-xl'>Preço Médio: R$ {item.avgPrice}</p>
-					<p className='textfield'>{item.description}</p>
-				</fieldset>
-			</div>
+		<div className="w-80 h-52 rounded-lg" style={{ backgroundImage: `url(${item.image})` }} onClick={onClick} >
+			<fieldset className="flex flex-col justify-center">
+				<h2 className="textfield font-bold text-2xl">{item.name}</h2>
+				<p className="textfield font-bold text-xl">Preço Médio: R$ {item.avgPrice}</p>
+				<p className="textfield">{item.description}</p>
+			</fieldset>
 		</div>
 	)
 }
