84c27647977a16070470b787da24ee06aaf10975
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-17 23:01:25 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,26 @@
-import useSWR from 'swr'
-import { useState } from 'react'
-import { fetcher2 } from './config/defaults'
-import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css'
-import { Layout } from './components/Layout'
-import { ItemCard } from './components/ItemCard'
-import { ItemSelect } from './components/ItemSelect'
-import { ItemDetails } from './components/ItemDetails'
+import Home from './pages/Home'
+import Items from './pages/Items'
+import Contractors from './pages/Contractors'
+import React, { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 
-// const url = '/api/graphql'
-const query = `{
-  allItems {
-    name
-    description
-    avgPrice
-    image
-    banner
-    unitOfMeasurement
-  }
-}
-`
+export default function App() {
+  const location = useLocation();
 
-function App() {
-  const { data: inventory, error, isValidating, mutate } = useSWR(query, fetcher2);
-  const [currentItem, setCurrentItem] = useState(0);
-
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  }
-
-  const handleClick = (itemIndex) => {
-    setCurrentItem(itemIndex)
-  }
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
 
   return (
-    <Layout>
-      <div>
-        <p><b>FIQUE A VONTADE PARA RETIRAR DE NOSSO SITE OS SERVIÇOS QUE ACHAR INCONVENIENTE</b></p>
-        <ItemSelect parentRef={{mutate}} ></ItemSelect>
-      </div>
-
-      <div>{
-        (inventory) ?
-        <Carousel responsive={responsive}>{  inventory.data.allItems.map(
-            (item, index) => <ItemCard item={item} index={index} parentRef={{handleClick}} key={item.id} />
-        )}
-        </Carousel> :
-        <p>No items found</p>
-      }
-      </div>
-
-      <div>{
-        (inventory) ?
-        <ItemDetails item={inventory.data.allItems[currentItem]} /> :
-        <p>No items found</p>
-      }
-      </div>
-    </Layout>
+    <>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/contractors" element={<Contractors />} />
+      </Routes>
+    </>
   )
 }
-
-export default App
